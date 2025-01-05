1d6621c4f1b53612624dc6707cd063aca48c1304
AlexCajeFelix <alexcaje2005@gmail.com>
2025-01-05 13:11:23 -0300

    Deixando Clean

--- main.c
+++ main.c
@@ -4,34 +4,28 @@
 #define MAX 100
 int AcessarFuncao;
 int Voltar;
+
 struct Cards
 {
     int id;
     char Titulo[20];
     char Descricao[20];
 };
-
-
 struct Bd
 {
     struct Cards Tarefas[10];
     int ContadorDeTarefas;
     char TempArray[10];
 };
-
 struct Bd Banco;
 
-
 VoltarPagAnterior() {
-
     printf("Envie 1 pra voltar e 0 para continuar criando :\n");
     scanf_s("%d", &Voltar);
     if (Voltar == 1) {
         main();
     }
 }
-//subindo correções 
-
 CriarTarefa() {
     printf("Digite o titulo que voce deseja criar:\n");
     fgets(Banco.Tarefas[Banco.ContadorDeTarefas].Titulo, sizeof(Banco.Tarefas[Banco.ContadorDeTarefas].Titulo), stdin);
@@ -46,65 +40,46 @@ EditarTarefa() {
     int LocalizaEdit;
     printf("-------------------- Editar -------------------\n");
     printf("Digite o id do card a ser alterado\n");
-    
     scanf_s("%d", &LocalizaEdit);
-    
     while (getchar() != '\n');
 
     for (int i = 0; i < Banco.ContadorDeTarefas; i++) {
         if (LocalizaEdit == Banco.Tarefas[i].id) {
             printf("Digite o titulo que voce deseja editar:\n");
-          
             fgets(Banco.Tarefas[i].Titulo, sizeof(Banco.Tarefas[i].Descricao), stdin);
-
             printf("Digite a descricao que voce deseja editar:\n");
-          
             fgets(Banco.Tarefas[i].Descricao, sizeof(Banco.Tarefas[i].Descricao), stdin);
-     
         }
     }
     VoltarPagAnterior();
-
 }
 Excluir() {
-
     printf("--------------------Excluir -------------------\n");
-    //Vou achar se o id existe e isolar o indice
-    // a ideia e criar um array e colocar os que não vão ser excluidos la e deixar o que vai ser excluido de fora 
-    // depois jogar desse array pro array global 
     printf("Digite o ID desejado para excluir: ");
     int ExcluirId;
-    scanf_s("%d", &ExcluirId);  // Usando scanf ao invés de scanf_s
-
+    scanf_s("%d", &ExcluirId);  
     int i, j = 0;
     int tarefaEncontrada = 0;
 
-    // Percorrendo as tarefas e excluindo a tarefa com o ID correspondente
     for (i = 0; i < Banco.ContadorDeTarefas; i++) {
         if (Banco.Tarefas[i].id != ExcluirId) {
-            // Copiar as tarefas restantes para o array sem excluir
+          
             Banco.Tarefas[j++] = Banco.Tarefas[i];
         }
         else {
-            tarefaEncontrada = 1;  // Marca que a tarefa foi encontrada e excluída
+            tarefaEncontrada = 1; 
         }
     }
-
-    // Atualizando o contador de tarefas após a exclusão
     if (tarefaEncontrada) {
-        Banco.ContadorDeTarefas = j;  // Ajusta o contador de tarefas
+        Banco.ContadorDeTarefas = j; 
         printf("Tarefa com ID %d foi excluída com sucesso!\n", ExcluirId);
     }
     else {
         printf("Tarefa com ID %d não encontrada.\n", ExcluirId);
     }
     VoltarPagAnterior();
-  
 }
 VisualizarTodos() {
-    // visualizar tem q mostrar todos que estão criado
-    //receber um card por card de cada indice e imprimir 
-
     printf("--------------------Vsualizar todos -------------------\n");
     for (int i = 0; i < Banco.ContadorDeTarefas; i++) {
         printf("Id %i\n", Banco.Tarefas[i].id);
@@ -116,10 +91,8 @@ VisualizarTodos() {
 VisualizarporID() {
     int LocalizaId;
     printf("--------------------Vsualizar por ID -------------------\n");
-
     printf("Digite o ID desejado\n");
     scanf_s("%d", &LocalizaId);
-
     if (LocalizaId == Banco.Tarefas[LocalizaId].id) {
         printf("Id %i\n", Banco.Tarefas[LocalizaId].id);
         printf("Titulo %s\n", Banco.Tarefas[LocalizaId].Titulo);
@@ -130,7 +103,6 @@ VisualizarporID() {
         printf("Esse id não existe!");
     }
     VoltarPagAnterior();
-
 }
 VisualizarporDescricao() {
     char FiltarDescricao[10];
@@ -150,9 +122,6 @@ VisualizarporDescricao() {
     }
     VoltarPagAnterior();
 }
-
-
-
 int main()
 {
     system("cls");
